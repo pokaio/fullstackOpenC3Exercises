377de42b824ddd89c2fa9e75bbeae5065c32aa51
Thai <trithai.t@gmail.com>
2022-08-03 12:47:26 +0200

    testing getUrl localhost

---
+++ build/precache-manifest.1414b3c3f378d8b81661730139136c07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7f408262d8016dd9a8a",
+    "url": "/static/css/main.6e19a057.chunk.css"
+  },
+  {
+    "revision": "c7f408262d8016dd9a8a",
+    "url": "/static/js/main.c7f40826.chunk.js"
+  },
+  {
+    "revision": "5661966288ab502ac675",
+    "url": "/static/js/1.56619662.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1f67d1026b0b4887c8433da2061a2c1",
+    "url": "/index.html"
+  }
+];
